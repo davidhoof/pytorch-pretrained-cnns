a16677cd5dd3ad56c2f0456a7013e4f9bda8de2c
davidhoof <DHoof@gmx.de>
2022-06-06 12:21:13 +0200

    Added sweep dataset and output handle

--- wandb_sweep.py
+++ wandb_sweep.py
@@ -15,8 +15,14 @@ def main(args):
     with open(args['configuration_file'], "r") as stream:
         config = yaml.safe_load(stream)
 
+    wandb_config = config['wandb']
     setup = config['setup']
     hyperparameters = config['hyperparameters']
+
+    if not os.path.exists(setup['data_dir']):
+        os.mkdir(setup['data_dir'])
+    if not os.path.exists(setup['output_dir']):
+        os.mkdir(setup['output_dir'])
 
     env = Environment(
         loader=PackageLoader("wandb_sweep")
@@ -25,24 +31,32 @@ def main(args):
 
     for dataset in setup['datasets']:
         project_name = setup['project_format_string'].format(dataset=dataset)
-        if not os.path.exists(os.path.join(setup['output_dir'], project_name)):
-            os.mkdir(os.path.join(setup['output_dir'], project_name))
+        if not os.path.exists(os.path.join(wandb_config['sweep_dir'], project_name)):
+            os.mkdir(os.path.join(wandb_config['sweep_dir'], project_name))
         for model in setup['models']:
             for checkpoint in setup['checkpoints']:
                 sweep_name = f"{model}{checkpoint['dataset_trained_on']}"
-                save_folder = os.path.join(setup['output_dir'], project_name, model)
+                save_folder = os.path.join(wandb_config['sweep_dir'], project_name, model)
                 if not os.path.exists(save_folder):
                     os.mkdir(save_folder)
+
                 with open(os.path.join(save_folder, f"{sweep_name}.py"), "w+") as f:
                     f.write(
-                        template.render(dataset=dataset, model=model, checkpoint=checkpoint, project_name=project_name))
+                        template.render(
+                            dataset=dataset,
+                            model=model,
+                            checkpoint=checkpoint,
+                            project_name=project_name,
+                            data_dir=os.path.abspath(setup['data_dir']),
+                            output_dir=os.path.abspath(setup['output_dir']))
+                    )
 
                 hyperparameters.update({"name": sweep_name})
                 hyperparameters.update({"program": os.path.join(save_folder, f"{sweep_name}.py")})
                 sweep_id = wandb.sweep(hyperparameters, project=project_name)
 
-                with open(os.path.join(setup['output_dir'], project_name, "sweep_agent_commands.txt"), "a+") as f:
-                    f.write(f"wandb agent {setup['username']}/{project_name}/{sweep_id}\n")
+                with open(os.path.join(wandb_config['sweep_dir'], project_name, "sweep_agent_commands.txt"), "a+") as f:
+                    f.write(f"wandb agent {wandb_config['username']}/{project_name}/{sweep_id}\n")
 
 
 if __name__ == "__main__":
