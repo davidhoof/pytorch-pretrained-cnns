1a5bc9131a200a80f6866288c0e98587b354b8d6
davidhoof <DHoof@gmx.de>
2022-06-06 15:25:08 +0200

    Added missing default parameters into sweep config

--- wandb_sweep.py
+++ wandb_sweep.py
@@ -1,8 +1,10 @@
+import glob
 import os
 from argparse import ArgumentParser
 
 import yaml
 from jinja2 import Environment, PackageLoader
+from datetime import datetime
 
 import wandb
 
@@ -30,26 +32,38 @@ def main(args):
     template = env.get_template("sweep_python.py.jinja2")
 
     for dataset in setup['datasets']:
-        project_name = setup['project_format_string'].format(dataset=dataset)
+        project_name = wandb_config['project_format_string'].format(dataset=dataset)
         if not os.path.exists(os.path.join(wandb_config['sweep_dir'], project_name)):
-            os.mkdir(os.path.join(wandb_config['sweep_dir'], project_name))
+            os.makedirs(os.path.join(wandb_config['sweep_dir'], project_name), exist_ok=True)
         for model in setup['models']:
-            for checkpoint in setup['checkpoints']:
+            for checkpoint in setup['checkpoint_inputs']:
                 sweep_name = f"{model}{checkpoint['dataset_trained_on']}"
                 save_folder = os.path.join(wandb_config['sweep_dir'], project_name, model)
                 if not os.path.exists(save_folder):
                     os.mkdir(save_folder)
 
+                defaults = dict(setup)
+                defaults['classifier'] = model
+                del defaults['models']
+                defaults['dataset'] = dataset
+                del defaults['datasets']
+                defaults['load_checkpoint'] = os.path.abspath(glob.glob(
+                    os.path.join(
+                        setup['output_dir'],
+                        f"/{checkpoint['dataset_trained_on']}/{model}/version_{checkpoint['version']}/checkpoints/*.ckpt")
+                )[0])
+                del defaults['checkpoint_inputs']
+                defaults['data_dir'] = os.path.abspath(setup['data_dir'])
+                defaults['output_dir'] = os.path.abspath(setup['output_dir'])
+                defaults['wandb'] = project_name
+
                 with open(os.path.join(save_folder, f"{sweep_name}.py"), "w+") as f:
                     f.write(
                         template.render(
-                            dataset=dataset,
-                            model=model,
-                            checkpoint=checkpoint,
-                            project_name=project_name,
-                            data_dir=os.path.abspath(setup['data_dir']),
-                            output_dir=os.path.abspath(setup['output_dir']),
-                            sys_path=os.path.abspath(os.getcwd())
+                            sys_path=os.path.abspath(os.getcwd()),
+                            type=type,
+                            str=str,
+                            defaults=defaults
                         )
                     )
 
@@ -57,7 +71,9 @@ def main(args):
                 hyperparameters.update({"program": os.path.join(save_folder, f"{sweep_name}.py")})
                 sweep_id = wandb.sweep(hyperparameters, project=project_name)
 
-                with open(os.path.join(wandb_config['sweep_dir'], project_name, "sweep_agent_commands.txt"), "a+") as f:
+                with open(os.path.join(wandb_config['sweep_dir'], project_name,
+                                       f"sweep_agent_commands{datetime.now().strftime('%d_%m_%Y_%H_%M')}.txt"),
+                          "a+") as f:
                     f.write(f"wandb agent {wandb_config['username']}/{project_name}/{sweep_id}\n")
 
 
